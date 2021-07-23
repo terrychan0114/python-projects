5c7e1c710239ad288ed312be2207dcd1b6c58f3b
Terry Chan <yychan@bu.edu>
2021-07-23 14:48:52 -0400

    Added 2 test python scripts

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import tkinter as tk
+import random
+
+def roll_die():
+    roll_value["text"] = str(random.randint(1,6))
 
 window = tk.Tk()
 
-for i in range(3):
-    for j in range(3):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        frame.grid(row=i, column=j)
-        label = tk.Label(master=frame, text=f"Row{i}\nColumn{j}")
-        label.pack()
+window.rowconfigure([0,1], minsize=150, weight=1)
+window.columnconfigure(0, minsize=150, weight=1)
+
+roll = tk.Button(master=window, text="Roll",command=roll_die)
+roll.grid(row=0, column=0, sticky="nsew")
+
+roll_value = tk.Label(master=window, text="Start")
+roll_value.grid(row=1, column=0)
 
 window.mainloop()
